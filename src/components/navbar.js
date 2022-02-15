9483f0f1ac237dfa941f25ecdf1f9c6a2193363d
Luan Saboia <luan.saboia@gmail.com>
2022-02-15 15:16:35 -0300

    implementado campos tela cadastro

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,20 +17,10 @@ function Navbar(){
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Cadastro</a>
+          <a className="nav-link" href="#/cadastro-produtos">Cadastro</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Consulta</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="#">Action</a>
-            <a className="dropdown-item" href="#">Another action</a>
-            <a className="dropdown-item" href="#">Something else here</a>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="#">Separated link</a>
-          </div>
         </li>
       </ul>
     </div>
--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -33,9 +33,37 @@ export default class CadastroProduto extends React.Component{
                   <label>Descrição: </label>
                   <textarea className="form-control"/>
               </div>
-              
+
             </div>
           </div>
+
+          <div className="row">
+
+            <div className="col-md-6">
+              <div className="form-group">
+                  <label>Preço: *</label>
+                  <input type="text" className="form-control"/>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                  <label>Fornecedor: *</label>
+                  <input type="text" className="form-control"/>
+              </div>
+            </div>
+            
+          </div>
+
+          <div className="row">
+            <div className="col-md-1">
+              <button className="btn btn-success" >Salvar</button>
+            </div>
+            <div className="col-md-1">
+              <button className="btn btn-primary" >Limpar</button>
+            </div>
+          </div>
+
         </div>
       </div>
     )
